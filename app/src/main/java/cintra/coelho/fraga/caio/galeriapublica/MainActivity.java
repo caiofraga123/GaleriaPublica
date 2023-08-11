b4de92d72ef4dbcf5dc4522772d9ebb2c8f0c995
20211tiimi0010 <caiofcintra@gmail.com>
2023-08-11 09:57:38 -0300

    Initial commit

---
+++ app/src/main/java/cintra/coelho/fraga/caio/galeriapublica/MainActivity.java
@@ -0,0 +1,14 @@
+package cintra.coelho.fraga.caio.galeriapublica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
