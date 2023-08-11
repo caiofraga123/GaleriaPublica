94fb342297f5ea3ab2e0b8dd4a63d7484facbea8
20211tiimi0010 <caiofcintra@gmail.com>
2023-08-11 11:04:48 -0300

    Página 9

---
+++ app/src/main/java/cintra/coelho/fraga/caio/galeriapublica/MainViewModel.java
@@ -0,0 +1,8 @@
+package cintra.coelho.fraga.caio.galeriapublica;
+
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+
+    int navigationOpSelected = R.id.gridVewOp;
+}
