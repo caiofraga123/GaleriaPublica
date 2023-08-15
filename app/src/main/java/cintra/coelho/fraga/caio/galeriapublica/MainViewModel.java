48df66bcca556d8cd39e0cb7e34c01758a1939f5
20211tiimi0010 <caiofcintra@gmail.com>
2023-08-15 11:03:51 -0300

    Página 13

--- app/src/main/java/cintra/coelho/fraga/caio/galeriapublica/MainViewModel.java
+++ app/src/main/java/cintra/coelho/fraga/caio/galeriapublica/MainViewModel.java
@@ -1,8 +1,25 @@
 package cintra.coelho.fraga.caio.galeriapublica;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+
+import java.io.Closeable;
 
 public class MainViewModel extends AndroidViewModel {
 
     int navigationOpSelected = R.id.gridVewOp;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public int getNavigationOpSelected() {
+        return navigationOpSelected;
+    }
+
+    public void setNavigationOpSelected(int navigationOpSelected) {
+        this.navigationOpSelected = navigationOpSelected;
+    }
 }
